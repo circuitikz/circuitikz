fdc160477788bec78aa303985202f7504fa4a95a
Stefan Lindner <stefan.lindner@gmail.com>
2016-05-27 16:51:46 +0200

    simplified deploy-script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -5,59 +5,46 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  make manual-git 
+	make manual-git 
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    doCompile
-    exit 0
+	echo "Skipping deploy; just doing a build."
+	doCompile
+	exit 0
 fi
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+# ENCRYPTION_LABEL is set at .travis.yml
+openssl aes-256-cbc -K encrypted_${ENCRYPTION_LABEL}_key -iv encrypted_${ENCRYPTION_LABEL}_iv -in tools/deploy_key.enc -out tools/deploy_key -d
+chmod 600 tools/deploy_key
+eval `ssh-agent -s`
+ssh-add tools/deploy_key
 
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-cd tools
-openssl aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
-cd ..
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH
-cd ..
-
-# Clean out existing contents
-#rm -rf out/**/* || exit 0
-#rm -rf out/*||exit 0
+git clone $REPO --singlebranch --depth=1 --branch=$TARGET_BRANCH out
 
 # Run our compile script
 doCompile
 
 cp circuitikzgit.sty out/
 cp circuitikzmanualgit.pdf out/
-# Now let's go have some fun with the cloned repo
+
+# Now change to gh-pages folder and check in compiled files
 cd out
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
-ls
-git add circuitikzgit.sty
-git add circuitikzmanualgit.pdf
+
+git add .
 git status
 
 # Commit the "changes", i.e. the new version.
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 git push $SSH_REPO $TARGET_BRANCH
-exit 0
