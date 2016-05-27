3f3d06a05c5aff5f1450a431ff9a784fc1cfe90b
Stefan Lindner <stefan.lindner@gmail.com>
2016-05-27 17:24:59 +0200

    redesign of travis code

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,47 +4,28 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
-function doCompile {
-	make manual-git 
-}
-
-# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-	echo "Skipping deploy; just doing a build."
-	doCompile
-	exit 0
-fi
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-# ENCRYPTION_LABEL is set at .travis.yml
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K ${ENCRYPTED_KEY} -iv ${ENCRYPTED_IV} -in tools/deploy_key.enc -out .deploy_key -d
-chmod 600 .deploy_key
-eval `ssh-agent -s`
-ssh-add .deploy_key
-
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+	echo "Skipping deploy; just done a build."
+	exit 0
+fi
 
 # Clone the existing gh-pages for this repo into out/
 git clone $REPO --single-branch --depth=1 --branch $TARGET_BRANCH out
-
-# Run our compile script
-doCompile
 
 cp circuitikzgit.sty out/
 cp circuitikzmanualgit.pdf out/
 
 # Now change to gh-pages folder and check in compiled files
 cd out
+
+git config user.name "Travis CI"
+git config user.email "travis@circuit.tikz"
 
 git add .
 git status
