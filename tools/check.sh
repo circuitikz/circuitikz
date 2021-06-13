675e251778dd5c8ac2a8d916381748e6623e2f68
Romano Giannetti <romano@rgtti.com>
2021-06-13 11:58:07 +0200

    Fix deploy

--- tools/check.sh
+++ tools/check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
+SOURCE_BRANCH="master"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
